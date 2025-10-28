d2f107279733b025219ad9a834b2000e71722010
Celso Gabriel Prado <celsogabrielf@gmail.com>
2025-10-28 10:44:38 -0300

    feat: Implementation of BoustrophedonSegmentedHeuristic class in base_heuristic

--- agricultural_csp/solver/constructive_heuristics/base_heuristics.py
+++ agricultural_csp/solver/constructive_heuristics/base_heuristics.py
@@ -141,3 +141,142 @@ class GreedyCoverageHeuristic(BaseConstructiveHeuristic):
 
         return solution
 
+class BoustrophedonSegmentedHeuristic(BaseConstructiveHeuristic):
+    
+    def __init__(self, instance: AgcspInstance, evaluator: AgcspEvaluator):
+        super().__init__(instance, evaluator)
+        
+    def _find_contiguous_segments(self, nodes: np.ndarray) -> list[np.ndarray]:
+        """
+        Takes a list of nodes in a column and divides it into contiguous segments.
+        """
+        if len(nodes) == 0:
+            return []
+
+        segments = []
+        current_segment = [nodes[0]]
+
+        for i in range(1, len(nodes)):
+            if nodes[i][0] - nodes[i-1][0] > 1.5:
+                segments.append(np.array(current_segment))
+                current_segment = []
+            current_segment.append(nodes[i])
+        
+        if current_segment:
+            segments.append(np.array(current_segment))
+            
+        return segments
+
+    def _generate_boustrophedon_path(self) -> AgcspSolution:
+        """
+        Generates an initial route in Boustrophedon pattern.
+        """
+        instance = self.instance
+        
+        visitable_indices = np.argwhere(instance._visitable_mask)
+        if visitable_indices.size == 0:
+            return AgcspSolution(path=[])
+        
+        visitable_nodes = visitable_indices + instance.min_coords
+
+        path_points = []
+        last_point = None
+        sprayerLength = instance.sprayer_length
+        step = sprayerLength - 1
+        
+        min_row, min_col = np.min(visitable_nodes, axis=0)
+        max_row, max_col = np.max(visitable_nodes, axis=0)
+
+        min_row += math.floor(sprayerLength / 2.0)
+        max_row -= math.floor(sprayerLength / 2.0)
+        
+        row = min_row
+
+        while row <= max_row:
+            nodes_in_column = visitable_nodes[(visitable_nodes[:, 1] >= row) & (visitable_nodes[:, 1] <= row)]
+
+            if len(nodes_in_column) == 0:
+                row += step
+                continue
+
+            nodes_in_column = nodes_in_column[np.argsort(nodes_in_column[:, 0])]
+            segments = self._find_contiguous_segments(nodes_in_column)
+
+            if not segments:
+                row += step
+                continue
+                
+            if last_point is None:
+                target_segment = segments[0]
+                entry_point = tuple(target_segment[0])
+                print(f"  Iniciando em {entry_point}")
+            else:
+                best_entry_point = None
+                min_dist = float('inf')
+                
+                for segment in segments:
+                    endpoints = [segment[0], segment[-1]]
+                    for point in endpoints:
+                        dist = np.linalg.norm(np.array(last_point) - point)
+                        if dist < min_dist:
+                            if self.evaluator.is_path_segment_valid(last_point, tuple(point)):
+                                min_dist = dist
+                                best_entry_point = tuple(point)
+                                target_segment = segment
+                
+                entry_point = best_entry_point
+
+            if entry_point is not None:
+                path_points.append(entry_point)
+                last_point = entry_point 
+
+                entry_point_arr = np.array(entry_point)
+
+                if np.array_equal(entry_point_arr, target_segment[0]):
+                    segment_to_add = target_segment
+                else:
+                    segment_to_add = target_segment[::-1]
+
+                internal_step = self.instance.sprayer_length 
+
+                for i in range(internal_step, len(segment_to_add), internal_step):
+                  current_node = tuple(segment_to_add[i])
+                  
+                  path_points.append(current_node)
+                  last_point = current_node 
+                    
+                final_node_of_segment = tuple(segment_to_add[-1])
+                
+                if len(segment_to_add) > 1 and not np.array_equal(np.array(last_point), final_node_of_segment):
+                  path_points.append(final_node_of_segment)
+                  last_point = final_node_of_segment
+                    
+            else:
+                last_point = None
+
+            row += step
+        
+        if not path_points:
+            return AgcspSolution(path=[])
+            
+        unique_path = [path_points[0]]
+        for i in range(1, len(path_points)):
+            p_current = np.array(path_points[i])
+            p_previous = np.array(path_points[i-1])
+            
+            if not np.array_equal(p_current, p_previous):
+                unique_path.append(path_points[i])
+                
+        solution = AgcspSolution(unique_path)
+        return solution
+
+    
+    def generate_initial_solution(self) -> AgcspSolution:
+      """
+      Orchestrates the generation of the solution
+      """
+      
+      solution = self._generate_boustrophedon_path()
+  
+      return solution
+
